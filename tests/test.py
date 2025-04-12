c0404a41cdfd2a3d9e77ffd181224d8023073266
Le Hien Hieu <hieulh.work@gmail.com>
2025-04-12 20:22:55 +0700

    Update tests

---
+++ tests/test.py
@@ -0,0 +1,10 @@
+import asyncio
+import cv2
+# from src.winrtocr import WinRTOCR
+
+# engine = WinRTOCR()
+
+# output_lines = asyncio.run(engine.ocr("images/cnn_news.png", lang="en-US", detail_level='line'))
+        
+# o_img = engine.draw_ocr_result("images/cnn_news.png", output_lines, detail_level='line')
+# cv2.imwrite("images/ocr_cnn_news.png", o_img)
